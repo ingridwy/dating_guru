871a36e405be08edbbd68f535a656272cd99e5dd
Ingrid Cheng <bluebear589@gmail.com>
2020-03-02 15:59:56 +0000

    Add restaurant model

---
+++ db/migrate/20200302155650_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.string :opening_hour
+      t.string :phone_number
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
