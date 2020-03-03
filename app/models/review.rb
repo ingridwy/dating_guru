e099417d70a46c2d5f89fd6d0492d1249fb96d54
Ingrid Cheng <bluebear589@gmail.com>
2020-03-03 14:48:54 +0000

    index, show,set_restaurant and set_activity method created. booking index and show view page done

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant, optional: true
   belongs_to :activities, optional: true
+  validates :rating, presence: true
+  validates :content, presence: true
 end
-
