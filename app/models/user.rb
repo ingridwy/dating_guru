8b97d6b085da08ef7cf3a9762ec0c81fe67ba46b
Ingrid Cheng <bluebear589@gmail.com>
2020-03-05 12:41:36 +0000

    title tag modified, username added for users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :user_name, presence: true
   has_many :reviews
   has_many :bookings
+  has_one_attached :profile_photo
 end
